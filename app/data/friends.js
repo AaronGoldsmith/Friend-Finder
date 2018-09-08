3a7f97ef7127162ebbd0af0c6dc266f8a1f6b926
Aaron Goldsmith <agoldsmith@scu.edu>
2018-09-08 03:32:31 -0700

    updated dependencies added server.js friends.js and export .html files

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,59 +1,30 @@
 
+// var path = require('path')
+var FriendArray = [];
 function User(name,gender,age){
-   this.name = name;
-   this.gender = gender;
-   this.age = age;
-   this.image = undefined;
-   this.data = [];	
-}
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.image = undefined;
+	this.data = [];
 
-User.prototype.setImage = function(urls){
+	this.setImage = function(urls){
 		this.image = urls;
-}
-
-// get users
-
-var users = [];
-
-var deadpool = new User("Deadpool","Male",29);
-deadpool.setImage("https://i.imgur.com/zPtP8WA.jpg");
-users.push(deadpool);
-
-var spiderman = new User("Spiderman","Male",30);
-spiderman.setImage("https://imgur.com/t/toby_mcguire/leCicDT");
-users.push(spiderman);
-/* 
-for(var key in deadpool){
-var val = deadpool[key];
-  if(typeof val !== "function"){
-     console.log(key +":"+deadpool[key]);
-   }
-}
-console.log(" \n")
-for(var key in spiderman){
-var val = spiderman[key];
-
-  if(typeof val !== "function"){
-     console.log(key +":"+spiderman[key]);
-   }
-}
- */
-var friends = [deadpool,spiderman];
-
-function displayUsers(userList){
-	var userCard = "";
-	for(var user in userList){
-	   var friend = userList[user];
- 	   userCard += "<div class='card border-info mb-3' style='max-width: 18rem;'>"
-	   userCard += "<div class='card-header'>userID:"+friend.ID+"</div>"
-	   userCard += "  <div class='card-body text-info'>"
-	   userCard += "  <h5 class='card-title userName'>"+friend.name+ " ("+user.gender+")</h5>"
-	   userCard += "  <p class='card-text stats'>"+friend.age +"</p>"
-	   userCard += "</div></div> "
 	}
-		
-	$(".users").html(userCard)
-
 }
-displayUsers(users);
+var deadpool = new User("Deadpool","Male",29);
+	deadpool.setImage("https://i.imgur.com/zPtP8WA.jpg");
+	FriendArray.push(deadpool);
+	
+	var spiderman = new User("Spiderman","Male",30);
+	spiderman.setImage("https://imgur.com/t/toby_mcguire/leCicDT");
+	FriendArray.push(spiderman);
+
+module.exports = FriendArray;
+
+	
+	
+	
+	
+	
 
