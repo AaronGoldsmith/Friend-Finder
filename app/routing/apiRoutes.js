2f283057ce4baf1986fc41d0e6cb00928babda24
Aaron Goldsmith <agoldsmith@scu.edu>
2018-09-11 04:24:44 -0700

    fell asleep, woke up woke

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,8 +4,13 @@
    app.get("/api/friends", function(req, res) {
        res.json(friendsData);
     });
+  
    app.post("/api/friends", function(req, res) {
-      friendsData.push(req.body)
       res.json(true);
+      
+      friendsData.push(req.body)
+      // console.log(friendsData[minInd], minDiff)
+
    });
+  
 }
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.port || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
